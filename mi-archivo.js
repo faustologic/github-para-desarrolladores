fb9cb97a02fb7cb710411c0021bce288e3551968
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-04 11:36:49 -0500

    Agregado ;
    Se agrega un ; en la lìnea 6

--- mi-archivo.js
+++ mi-archivo.js
@@ -3,5 +3,5 @@ console.log("Este archivo fue editado");
 
 var btn = document.getElementsByClassName(".btn-member");
 btn.addEventListener(click, function(){
-    alert("has pulsado el botón")
+    alert("has pulsado el botón");
 });
