ec01adff647b020f2f9a13bee7b489c5ba39c728
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-04 11:28:35 -0500

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,7 @@
-alert("EDITADO");
+//alert("EDITADO");
 console.log("Este archivo fue editado");
+
+var btn = document.getElementsByClassName(".btn-member");
+btn.addEventListener(click, function(){
+    alert("has pulsado el botón")
+});
